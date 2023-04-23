40ec2f006ef3363cbaf0f4da558d8be4bc6ef99f
huhaoyang <huhaoyang0804@qq.com>
2023-04-23 22:47:57 +0200

    4.23

--- scripts/jemdoc.py
+++ scripts/jemdoc.py
@@ -238,6 +238,7 @@ def standardconf():
   [footerstart]
   <div id="footer">
   <div id="footer-text">
+    <a href="http://s11.flagcounter.com/more/jD"><img src="https://s11.flagcounter.com/mini/jD/bg_FFFFFF/txt_000000/border_CCCCCC/flags_0/" alt="Flag Counter" border="0"></a>
 
   [footerend]
   </div>
